ee8f3d2f98d04342fa903368ac08fc1846af8a50
Vishvajeet <vishvajeet6042gmail.com>
2026-07-22 11:28:50 +0530

    new sql project

---
+++ employee.sql
@@ -0,0 +1,57 @@
+CREATE database employee_db
+use employee_db
+
+
+CREATE TABLE employees (
+    emp_id INT IDENTITY(101,1) PRIMARY KEY,
+    fname VARCHAR(50) NOT NULL,
+    lname VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    job_title VARCHAR(50) NOT NULL, 
+    department VARCHAR(50),    
+    salary DECIMAL(10,2) DEFAULT 30000.00,
+    hire_date DATE NOT NULL DEFAULT CONVERT(date, GETDATE()),
+    city VARCHAR(50)
+);
+
+
+INSERT INTO employees
+(fname, lname, email, job_title, department, salary, hire_date, city)
+VALUES
+('Aarav', 'Sharma', 'aarav.sharma@example.com', 'Director', 'Management', 180000, '2019-02-10', 'Mumbai'),
+('Diya', 'Patel', 'diya.patel@example.com', 'Lead Engineer', 'Tech', 120000, '2020-08-15', 'Bengaluru'),
+('Rohan', 'Mehra', 'rohan.mehra@example.com', 'Software Engineer', 'Tech', 85000, '2022-05-20', 'Bengaluru'),
+('Priya', 'Singh', 'priya.singh@example.com', 'HR Manager', 'Human Resources', 95000, '2019-11-05', 'Mumbai'),
+('Arjun', 'Kumar', 'arjun.kumar@example.com', 'Data Scientist', 'Tech', 110000, '2021-07-12', 'Hyderabad'),
+('Ananya', 'Gupta', 'ananya.gupta@example.com', 'Marketing Lead', 'Marketing', 90000, '2020-03-01', 'Delhi'),
+('Vikram', 'Reddy', 'vikram.reddy@example.com', 'Sales Executive', 'Sales', 75000, '2023-01-30', 'Mumbai'),
+('Sameera', 'Rao', 'sameera.rao@example.com', 'Software Engineer', 'Tech', 88000, '2023-06-25', 'Pune'),
+('Ishaan', 'Verma', 'ishaan.verma@example.com', 'Recruiter', 'Human Resources', 65000, '2022-09-01', 'Mumbai'),
+('Kavya', 'Joshi', 'kavya.joshi@example.com', 'Product Designer', 'Design', 92000, '2021-04-18', 'Bengaluru'),
+('Zain', 'Khan', 'zain.khan@example.com', 'Sales Manager', 'Sales', 115000, '2019-09-14', 'Delhi'),
+('Nisha', 'Desai', 'nisha.desai@example.com', 'Jr. Data Analyst', 'Tech', 70000, '2024-02-01', 'Hyderabad'),
+('Aditya', 'Nair', 'aditya.nair@example.com', 'Marketing Analyst', 'Marketing', 68000, '2022-10-10', 'Delhi'),
+('Fatima', 'Ali', 'fatima.ali@example.com', 'Sales Executive', 'Sales', 78000, '2022-11-22', 'Mumbai'),
+('Kabir', 'Shah', 'kabir.shah@example.com', 'DevOps Engineer', 'Tech', 105000, '2020-12-01', 'Pune');
+
+SELECT * FROM employees
+
+
+-- AGGRECATE FUNCTION:-   (count,sum,min.max,avg)
+
+SELECT COUNT (emp_id) FROM employees
+SELECT MIN (salary) FROM employees
+SELECT MAX (salary) FROM employees
+SELECT AVG (salary) FROM employees
+
+
+-- GROUP BY
+SELECT department FROM employees GROUP BY department
+SELECT department, count (emp_id) FROM employees GROUP BY department
+
+SELECT city, count (emp_id)  FROM employees GROUP BY city
+
+SELECT city,SUM  (salary)  FROM employees GROUP BY city 
+
+SELECT salary, SUM (salary) FROM employees GROUP BY salary
+
